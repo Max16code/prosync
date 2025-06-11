dc091f4be838b5093590e0200f1ad5e853cd113c
max16code <amanzeikedi16@gmail.com>
2025-06-11 23:37:12 +0100

    moresexiest

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -15,17 +15,17 @@ export default function page() {
     
 <div className='flex gap-30 mt-20'>
 
-<div class="w-100 h-32 border-1 border-yellow-500 text-white flex items-center justify-center flex-col">
+<div className="w-100 h-32 border-1 border-yellow-500 text-white flex items-center justify-center flex-col">
   Crude Oil Distillation Unit Revamp
   <p className='text-green-200 mt-1'>Major Oil Refinery</p>
   <h3 className='mt-1 pl-3'>Process design and safety analysis for 150,000 BPD unit upgrade</h3>
 </div>
-<div class="w-100 h-32 border-1 border-yellow-500 text-white flex items-center justify-center flex-col ">
+<div className="w-100 h-32 border-1 border-yellow-500 text-white flex items-center justify-center flex-col ">
  Chemical Plant Safety Systems
   <p className='text-green-200 mt-1' >Petrochemical Facility</p>
   <h3 className='mt-1 pl-3'>SIL-rated safety instrumented systems design and implementation</h3>
 </div>
-<div class="w-100 h-32 border-1 border-yellow-500 text-white flex items-center justify-center flex-col">
+<div className="w-100 h-32 border-1 border-yellow-500 text-white flex items-center justify-center flex-col">
  Gas Processing Equipment
   <p className='text-green-200 mt-1'>Natural Gas Company</p>
   <h3 className='mt-1 pl-3'>Custom separator and compressor package design and procurement</h3>
